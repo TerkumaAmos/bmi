28911d36308042c82e353798fdbedc34633e1bc8
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-01 12:36:11 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        // textTheme: const TextTheme(),
+        // primaryColor: const Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
