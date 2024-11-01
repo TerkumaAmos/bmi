666c08a540e153f7e31dbd2ddbcb0a5f5e48c6d2
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-01 19:05:28 +0100

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -59,7 +59,7 @@ class _InputPageState extends State<InputPage> {
           ),
           Container(
             color: const Color(0xFFEB1555),
-            margin: const EdgeInsets.only(top: 10.0),
+            margin: const EdgeInsets.only(top: 20.0),
             width: double.infinity,
             height: bottomContainerHeight,
           )
