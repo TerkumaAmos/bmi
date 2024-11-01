b18486b8c097c61e7af39bd375bfae3e8ca5e8eb
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-02 00:51:51 +0100

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -83,7 +83,7 @@ class ReusableCard extends StatelessWidget {
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: Colour,
-        borderRadius: BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.circular(20.0),
       ),
     );
   }
