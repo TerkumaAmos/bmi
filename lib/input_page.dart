055ad36e262bc65f42eb1b48aae9fb00da860d2f
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-02 02:10:52 +0100

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: must_be_immutable, library_private_types_in_public_api, use_key_in_widget_constructors, prefer_const_constructors, non_constant_identifier_names, prefer_const_constructors_in_immutables
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
@@ -15,25 +13,28 @@ class InputPage extends StatefulWidget {
   _InputPageState createState() => _InputPageState();
 }
 
-Color maleCardColour = inactiveCardColour;
-Color femaleCardColour = inactiveCardColour;
-
-void updateColour = (int gender) {
-  if (gender == 1) {
-    if (maleCardColour == inactiveCardColour) {
-      maleCardColour = activeCardColour;
-    } else {
-      maleCardColour = inactiveCardColour;
-    }
-  }
-};
-
 class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  void updateColour(int gender) {
+    setState(() {
+      if (gender == 1) {
+        maleCardColour = (maleCardColour == inactiveCardColour)
+            ? activeCardColour
+            : inactiveCardColour;
+      } else if (gender == 2) {
+        femaleCardColour = (femaleCardColour == inactiveCardColour)
+            ? activeCardColour
+            : inactiveCardColour;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: const Color(0x0ffa0e21),
         title: const Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
@@ -45,12 +46,10 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
-                      setState(() {
-                        updateColour(1);
-                      });
+                      updateColour(1);
                     },
                     child: ReusableCard(
-                      Colour: inactiveCardColour,
+                      Colour: maleCardColour,
                       cardChild: IconContent(
                         icon: FontAwesomeIcons.mars,
                         label: 'MALE',
@@ -59,33 +58,25 @@ class _InputPageState extends State<InputPage> {
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                      Colour: inactiveCardColour,
+                  child: GestureDetector(
+                    onTap: () {
+                      updateColour(2);
+                    },
+                    child: ReusableCard(
+                      Colour: femaleCardColour,
                       cardChild: IconContent(
-                          icon: FontAwesomeIcons.venus, label: "FEMALE")),
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          // Expanded(
-          //   child: ReusableCard(
-          //     Colour: activeCardColour,
-          //   ),
-          // ),
           Expanded(
             child: Row(
-              children: const [
-                // Expanded(
-                //   child: ReusableCard(
-                //     Colour: activeCardColour,
-                //   ),
-                // ),
-                // Expanded(
-                //   child: ReusableCard(
-                //     Colour: activeCardColour,
-                //   ),
-                // )
-              ],
+              children: const [],
             ),
           ),
           Container(
@@ -93,7 +84,7 @@ class _InputPageState extends State<InputPage> {
             margin: const EdgeInsets.only(top: 20.0),
             width: double.infinity,
             height: bottomContainerHeight,
-          )
+          ),
         ],
       ),
     );
