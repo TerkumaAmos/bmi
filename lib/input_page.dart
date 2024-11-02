4c8672ca067839490c83f4ae31991cbe2b62d61b
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-02 01:13:49 +0100

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -26,40 +28,59 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    Colour: const Color(0xFF1D1E33),
+                    Colour: activeCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E9E),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                Expanded(
-                  child: ReusableCard(
-                    Colour: const Color(0xFF1D1E33),
-                  ),
-                ),
+                // Expanded(
+                //   child: ReusableCard(
+                //     Colour: activeCardColour,
+                //   ),
+                // ),
               ],
             ),
           ),
-          Expanded(
-            child: ReusableCard(
-              Colour: const Color(0xFF1D1E33),
-            ),
-          ),
+          // Expanded(
+          //   child: ReusableCard(
+          //     Colour: activeCardColour,
+          //   ),
+          // ),
           Expanded(
             child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    Colour: const Color(0xFF1D1E33),
-                  ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    Colour: const Color(0xFF1D1E33),
-                  ),
-                )
+              children: const [
+                // Expanded(
+                //   child: ReusableCard(
+                //     Colour: activeCardColour,
+                //   ),
+                // ),
+                // Expanded(
+                //   child: ReusableCard(
+                //     Colour: activeCardColour,
+                //   ),
+                // )
               ],
             ),
           ),
           Container(
-            color: const Color(0xFFEB1555),
+            color: bottomContainerColour,
             margin: const EdgeInsets.only(top: 20.0),
             width: double.infinity,
             height: bottomContainerHeight,
